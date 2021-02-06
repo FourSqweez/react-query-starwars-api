36c97ae041b8ef6266f9fb04f263e8ff91102512
FourSqweez <Kjeerasak@kkumail.com>
2021-02-07 00:41:08 +0700

    Add : add api, fetch data, show to template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar'
+import React, { useState } from 'react'
+import Planets from './components/Planets'
+import People from './components/People'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { QueryClient, QueryClientProvider } from 'react-query'
+const queryClient = new QueryClient()
 
 function App() {
+  const [page, setPage] = useState('planets')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <QueryClientProvider client={queryClient}>
+
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <div className="content">
+          <Navbar setPage={setPage} />
+          {page === 'planets' ? <Planets /> : <People />}
+        </div>
+      </div>
+      <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Planets.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import { useQuery } from 'react-query'
+import Planet from './Planet'
+
+const fetchPlanets = async (key, page) => {
+  // console.log(page)
+  const res = await fetch(`http://swapi.dev/api/planets/?page=${page}`)
+  return res.json()
+
+  /**
+   * * if Using axios
+   ** const {data} = await axios.get(`http://swapi.dev/api/planets/?page=${page}`)
+   ** return data
+   */ 
+  
+}
+
+const Planets = () => {
+  const [page, setPage] = useState(1)
+  const { data, status } = useQuery(
+    ['planets', page],
+    () => fetchPlanets('planets', page),
+    { keepPreviousData: true }
+  )
+  // console.log(data)
+  return (
+    <div>
+      <h2>Planets</h2>
+
+      {status === 'loading' && <div>Loading data...</div>}
+      {status === 'error' && <div>Error fetching data</div>}
+      {status === 'success' && (
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous page
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() => setPage((old) => (data?.next ? old + 1 : old))}
+            disabled={!data.next}
+          >
+            Next page
+          </button>
+
+          <div>
+            {data.results.map((planet) => (
+              <Planet key={planet.name} planet={planet} />
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default Planets
